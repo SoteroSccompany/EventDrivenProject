5c468804748569d554d03188fbb9c5aa27bdf4f0
Gabriel Sotero <64966688+SoteroSccompany@users.noreply.github.com>
2025-01-02 12:03:16 -0300

    ms ok

--- balance/dist/internal/event/balance-updated.event.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BalanceUpdatedEvent {
-    constructor(eventData) {
-        this.dataTimeOccured = new Date();
-        this.payload = eventData;
-    }
-}
-exports.default = BalanceUpdatedEvent;
